a7d9eea19a3a19cc325e81f29ba15dfb904074c6
viresh rawool <vireshrawool007@gmail.com>
2021-02-08 13:19:08 +0530

    modified:employeeWageComputation refactor code using switch case

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -4,19 +4,22 @@ public class employeeWageComputation {
                 int is_full_time = 8;
 		int is_part_time = 4;
 		int per_hour_wage = 20;
-		int present_fullTime = 2;
-		int present_partTime = 1;
+		final int present_fullTime = 2;
+		final int present_partTime = 1;
 		double salary = 0;
                 // computation
                 double empcheck = Math.floor(Math.random() * 10 ) % 3;
-                if ( empcheck == present_fullTime ){
-			salary = per_hour_wage * is_full_time;
-                        System.out.println("employee is present ");
-		}else if ( empcheck == present_partTime ){
-			salary = per_hour_wage * is_part_time;
-                        System.out.println("employee is present ");
-
-                }else{
+		int check=(int)empcheck;
+		switch (check) {
+			case present_fullTime:
+				salary = per_hour_wage * is_full_time;
+                        	System.out.println("employee is present ");
+				break;
+			case present_partTime:
+				salary = per_hour_wage * is_part_time;
+        	                System.out.println("employee is present ");
+				break;
+			default:
                         System.out.println("employee is abscent ");
 		}
 		System.out.println("salary : " + salary);
