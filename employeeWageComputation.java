42917a7bb22d81d5685e33f9ce854ed25c64d63c
viresh rawool <vireshrawool007@gmail.com>
2021-02-08 11:55:59 +0530

    modified: employeeWageComputation by adding if else to check absent or present

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,7 +1,13 @@
 public class employeeWageComputation {
         public static void main(String[] args) {
-		System.out.println("welcome to employee computation in java ");
+                // constants
+                int is_full_time = 1;
+                // computation
+                double empcheck = Math.floor(Math.random() * 10 ) % 2;
+                if ( empcheck == is_full_time )
+                        System.out.println("employee is present");
+                else
+                        System.out.println("employee is abscent ");
         }
 }
-
 
