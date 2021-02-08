f2733d6cea97de5b14a787e0f1f18b93e46b947f
viresh rawool <vireshrawool007@gmail.com>
2021-02-08 15:01:36 +0530

    modified:the loop using while condition to calulate for max day or max hours wage

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -6,20 +6,26 @@ public class employeeWageComputation {
 		int per_hour_wage = 20;
 		final int present_fullTime = 2;
 		final int present_partTime = 1;
+		int work_hours = 0;
+		int work_days = 0;
 		double salary = 0;
                 // computation
-		for (int i=0; i<=20; i++ ){
+		while ( work_hours < 100 && work_days < 20 ){
                 	double empcheck = Math.floor(Math.random() * 10 ) % 3;
 			int check=(int)empcheck;
 			switch (check) {
 				case present_fullTime:
 					salary += per_hour_wage * is_full_time;
+					work_hours += is_full_time;
+					work_days += 1;
 					break;
 				case present_partTime:
 					salary += per_hour_wage * is_part_time;
+					work_hours += is_part_time;
+					work_days += 1;
 					break;
 				default:
-					i--;
+
 			}
 		}
 		System.out.println("salary : " + salary);
