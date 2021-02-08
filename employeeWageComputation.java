fd0c7f9b9bd06d213e5d542863245c304fa7e9a8
viresh rawool <vireshrawool007@gmail.com>
2021-02-08 12:21:35 +0530

    modified: employeeWageComputation by adding the calculation for per hour wage

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,13 +1,20 @@
 public class employeeWageComputation {
         public static void main(String[] args) {
                 // constants
-                int is_full_time = 1;
+                int is_full_time = 8;
+		int per_hour_wage = 20;
+		int present = 1;
+		double salary = 0;
                 // computation
                 double empcheck = Math.floor(Math.random() * 10 ) % 2;
-                if ( empcheck == is_full_time )
-                        System.out.println("employee is present");
-                else
+                if ( empcheck == present ){
+			salary = per_hour_wage * is_full_time;
+                        System.out.println("employee is present ");
+			System.out.println(salary);
+                }else{
                         System.out.println("employee is abscent ");
+			System.out.println(salary);
+		}
         }
 }
 
