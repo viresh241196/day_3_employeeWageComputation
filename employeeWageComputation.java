2ed580f52433ffade9102453855f51a192d0408c
viresh rawool <vireshrawool007@gmail.com>
2021-02-08 12:33:05 +0530

    modified: employeeWageComputation by adding the feature of part time wage

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -2,19 +2,25 @@ public class employeeWageComputation {
         public static void main(String[] args) {
                 // constants
                 int is_full_time = 8;
+		int is_part_time = 4;
 		int per_hour_wage = 20;
-		int present = 1;
+		int present_fullTime = 2;
+		int present_partTime = 1;
 		double salary = 0;
                 // computation
-                double empcheck = Math.floor(Math.random() * 10 ) % 2;
-                if ( empcheck == present ){
+                double empcheck = Math.floor(Math.random() * 10 ) % 3;
+                if ( empcheck == present_fullTime ){
 			salary = per_hour_wage * is_full_time;
                         System.out.println("employee is present ");
-			System.out.println(salary);
+		}else if ( empcheck == present_partTime ){
+			salary = per_hour_wage * is_part_time;
+                        System.out.println("employee is present ");
+
                 }else{
                         System.out.println("employee is abscent ");
-			System.out.println(salary);
 		}
+		System.out.println("salary : " + salary);
+
         }
 }
 
