bc2766967e0583aa701ed03d69502dfd82a8cdac
viresh241196 <70256383+viresh241196@users.noreply.github.com>
2021-02-10 17:40:30 +0530

    modified :indentation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,11 +1,9 @@
-public class employeeWageComputation {
+public class EmployeeWageComputation {
         public static void main(String[] args) {
-                // constants
                 int is_full_time = 8;
 		int per_hour_wage = 20;
 		int present = 1;
 		double salary = 0;
-                // computation
                 double empcheck = Math.floor(Math.random() * 10 ) % 2;
                 if ( empcheck == present ){
 			salary = per_hour_wage * is_full_time;
