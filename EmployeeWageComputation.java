a109d23aac23073cb543d69055ded9a21cdcc196
viresh241196 <70256383+viresh241196@users.noreply.github.com>
2021-02-11 11:57:47 +0530

    Modified: indentation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,31 +1,32 @@
 public class EmployeeWageComputation {
-    	public static final int present_fullTime = 2;
-    	public static final int present_partTime = 1;
-    	public static final int is_full_time = 8;
-    	public static final int is_part_time = 4;
-    	public static final int per_hour_wage = 20;
-	public static void main(String[] args) {
-		int work_hours = 0;
-		int work_days = 0;
-		double salary = 0;
-		while ( work_hours < 100 && work_days < 20 ){
-                	double empcheck = Math.floor(Math.random() * 10 ) % 3;
-			int check=(int)empcheck;
-			switch (check) {
-				case present_fullTime:
-					salary += per_hour_wage * is_full_time;
-					work_hours += is_full_time;
-					work_days += 1;
-					break;
-				case present_partTime:
-					salary += per_hour_wage * is_part_time;
-					work_hours += is_part_time;
-					work_days += 1;
-					break;
-				default:
-			}
-		}
-		System.out.println("salary : " + salary);
+    public static final int present_fullTime = 2;
+    public static final int present_partTime = 1;
+    public static final int is_full_time = 8;
+    public static final int is_part_time = 4;
+    public static final int per_hour_wage = 20;
+
+    public static void main(String[] args) {
+        int work_hours = 0, work_days = 0;
+        double salary = 0;
+        while (work_hours < 100 && work_days < 20) {
+            work_days++;
+            double empcheck = Math.floor(Math.random() * 10) % 3;
+            int check = (int) empcheck;
+            switch (check) {
+                case present_fullTime:
+                    work_hours += is_full_time;
+                    break;
+                case present_partTime:
+                    work_hours += is_part_time;
+                    break;
+                default:
+                    work_hours += 0;
+            }
+            System.out.println("Day"+work_days + " Total working hrs "+work_hours);
         }
+        salary = work_hours * per_hour_wage;
+        System.out.println("total working hours " + work_hours + " total working days " + work_days);
+        System.out.println("salary : " + salary);
+    }
 }
 
