068db0286dba82190ac13e77a62f5d8fafa0e23e
viresh rawool <vireshrawool007@gmail.com>
2021-02-11 13:23:46 +0530

    Modified: using object concept

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,37 +3,32 @@ public class EmployeeWageComputation {
     public static final int present_partTime = 1;
     public static final int is_full_time = 8;
     public static final int is_part_time = 4;
-    public static final int per_hour_wage = 20;
 
-    static double calulate_salary(double salary, int per_hour_wage, int work_time) {
-        salary += per_hour_wage * work_time;
-        return salary;
-    }
-    static int calulate_work_hours(int work_hours, int full_or_part_time) {
-        work_hours += full_or_part_time;
-        return work_hours;
-    }
-    public static void main(String[] args) {
+    public static double calulateSalary(String company,int per_hour_wage,int max_work_days,int max_work_hours){
         int work_hours = 0, work_days = 0;
         double salary = 0;
-        while (work_hours < 100 && work_days < 20) {
+        while (work_hours < max_work_hours && work_days < max_work_days) {
+            work_days++;
             double empcheck = Math.floor(Math.random() * 10) % 3;
             int check = (int) empcheck;
             switch (check) {
                 case present_fullTime:
-                    salary = calulate_salary(salary, per_hour_wage, is_full_time);
-                    work_hours = calulate_work_hours(work_hours, is_full_time);
-                    work_days += 1;
+                    work_hours += is_full_time;
                     break;
                 case present_partTime:
-                    salary = calulate_salary(salary, per_hour_wage, is_part_time);
-                    work_hours = calulate_work_hours(work_hours, is_part_time);
-                    work_days += 1;
+                    work_hours += is_part_time;
                     break;
                 default:
+                    work_hours += 0;
             }
+            System.out.println("Day"+work_days + " Total working hrs "+work_hours);
         }
+        salary = work_hours * per_hour_wage;
         System.out.println("total working hours " + work_hours + " total working days " + work_days);
         System.out.println("salary : " + salary);
+        return salary;
+    }
+    public static void main(String[] args) {
+        calulateSalary("bmw",25,25,100);
     }
 }
