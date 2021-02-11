38037ce7cbd8ad2242db58896fe37df9147736ad
viresh241196 <70256383+viresh241196@users.noreply.github.com>
2021-02-11 21:04:30 +0530

    Modified: Indentation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,9 +1,9 @@
 public class EmployeeWageComputation {
-    public static final int present_fullTime = 2;
-    public static final int present_partTime = 1;
-    public static final int is_full_time = 8;
-    public static final int is_part_time = 4;
-    public static final int per_hour_wage = 20;
+    public static final int PRESENT_FULL_TIME = 2;
+    public static final int PRESENT_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 8;
+    public static final int IS_PART_TIME = 4;
+    public static final int PER_HOUR_WAGE = 20;
 
     public static void main(String[] args) {
         int work_hours = 0, work_days = 0;
@@ -13,18 +13,18 @@ public class EmployeeWageComputation {
             double empcheck = Math.floor(Math.random() * 10) % 3;
             int check = (int) empcheck;
             switch (check) {
-                case present_fullTime:
-                    work_hours += is_full_time;
+                case PRESENT_FULL_TIME:
+                    work_hours += IS_FULL_TIME;
                     break;
-                case present_partTime:
-                    work_hours += is_part_time;
+                case PRESENT_PART_TIME:
+                    work_hours += IS_PART_TIME;
                     break;
                 default:
                     work_hours += 0;
             }
             System.out.println("Day"+work_days + " Total working hrs "+work_hours);
         }
-        salary = work_hours * per_hour_wage;
+        salary = work_hours * PER_HOUR_WAGE;
         System.out.println("total working hours " + work_hours + " total working days " + work_days);
         System.out.println("salary : " + salary);
     }
