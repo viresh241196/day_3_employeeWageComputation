6481f9ef1793bcc98a31637b1271111f4c31701b
viresh rawool <vireshrawool007@gmail.com>
2021-02-11 20:19:04 +0530

    Refactor:using oops concept

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,34 +1,48 @@
 public class EmployeeWageComputation {
-    public static final int present_fullTime = 2;
-    public static final int present_partTime = 1;
-    public static final int is_full_time = 8;
-    public static final int is_part_time = 4;
+    public static final int PRESENT_FULL_TIME = 2;
+    public static final int PRESENT_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 8;
+    public static final int IS_PART_TIME = 4;
 
-    public static double calulateSalary(String company,int per_hour_wage,int max_work_days,int max_work_hours){
+    private String company;
+    private int per_hours_wage;
+    private int max_work_days;
+    private int max_work_hours;
+
+    public EmployeeWageComputation(String company, int per_hour_wage, int max_work_days, int max_work_hours) {
+        this.company = company;
+        this.per_hours_wage = per_hour_wage;
+        this.max_work_days = max_work_days;
+        this.max_work_hours = max_work_hours;
+    }
+
+    public static double calulateSalary(EmployeeWageComputation obj) {
         int work_hours = 0, work_days = 0;
         double salary = 0;
-        while (work_hours < max_work_hours && work_days < max_work_days) {
+        while (work_hours < obj.max_work_hours && work_days < obj.max_work_days) {
             work_days++;
             double empcheck = Math.floor(Math.random() * 10) % 3;
             int check = (int) empcheck;
             switch (check) {
-                case present_fullTime:
-                    work_hours += is_full_time;
+                case PRESENT_FULL_TIME:
+                    work_hours += IS_FULL_TIME;
                     break;
-                case present_partTime:
-                    work_hours += is_part_time;
+                case PRESENT_PART_TIME:
+                    work_hours += IS_PART_TIME;
                     break;
                 default:
                     work_hours += 0;
             }
-            System.out.println("Day"+work_days + " Total working hrs "+work_hours);
+            System.out.println("Day" + work_days + " Total working hrs " + work_hours);
         }
-        salary = work_hours * per_hour_wage;
-        System.out.println("total working hours " + work_hours + " total working days " + work_days);
+        salary = work_hours * obj.per_hours_wage;
+        System.out.println("company " + obj.company + " total working hours " + work_hours + " total working days " + work_days);
         System.out.println("salary : " + salary);
         return salary;
     }
+
     public static void main(String[] args) {
-        calulateSalary("bmw",25,25,100);
+        EmployeeWageComputation dMart = new EmployeeWageComputation("DMart", 20, 25, 100);
+        calulateSalary(dMart);
     }
 }
