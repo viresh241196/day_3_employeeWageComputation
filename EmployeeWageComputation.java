b1c2bbd9fb689f96886777cbb86016946ac64e39
viresh241196 <70256383+viresh241196@users.noreply.github.com>
2021-02-11 21:01:52 +0530

    Modified: indentation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,9 +1,9 @@
 public class EmployeeWageComputation {
-    public static final int present_fullTime = 2;
-    public static final int present_partTime = 1;
-    public static final int is_full_time = 8;
-    public static final int is_part_time = 4;
-    public static final int per_hour_wage = 20;
+    public static final int PRESENT_FULL_TIME = 2;
+    public static final int PRESENT_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 8;
+    public static final int IS_PART_TIME = 4;
+    public static final int PER_HOUR_WAGE = 20;
 
     public static void main(String[] args) {
         int working_hr = 0;
@@ -12,18 +12,17 @@ public class EmployeeWageComputation {
             double empcheck = Math.floor(Math.random() * 10) % 3;
             int check = (int) empcheck;
             switch (check) {
-                case present_fullTime:
-                    working_hr += is_full_time;
+                case PRESENT_FULL_TIME:
+                    working_hr += IS_FULL_TIME;
                     break;
-                case present_partTime:
-                    working_hr += is_part_time;
+                case PRESENT_PART_TIME:
+                    working_hr += IS_PART_TIME;
                     break;
                 default:
                     working_hr += 0;
             }
         }
-        salary = per_hour_wage * working_hr;
+        salary = PER_HOUR_WAGE * working_hr;
         System.out.println("salary : " + salary);
     }
 }
-
