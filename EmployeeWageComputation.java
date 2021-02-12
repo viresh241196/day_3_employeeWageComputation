df29c79bccd00c2981975e1ac5763835fedfaf26
viresh rawool <vireshrawool007@gmail.com>
2021-02-12 12:32:59 +0530

    Refactor:using oops concept created object for each company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,14 +3,23 @@ public class EmployeeWageComputation {
     public static final int PRESENT_PART_TIME = 1;
     public static final int IS_FULL_TIME = 8;
     public static final int IS_PART_TIME = 4;
-    public static final int PER_HOURS_WAGE  = 20;
-    public static final int MAX_WORK_DAYS  = 20;
-    public static final int MAX_WORK_HOURS  = 100;
 
-    public static double calulateSalary() {
+    private String company;
+    private int per_hours_wage;
+    private int max_work_days;
+    private int max_work_hours;
+
+    public EmployeeWageComputation(String company, int per_hour_wage, int max_work_days, int max_work_hours) {
+        this.company = company;
+        this.per_hours_wage = per_hour_wage;
+        this.max_work_days = max_work_days;
+        this.max_work_hours = max_work_hours;
+    }
+
+    public static double calulateSalary(EmployeeWageComputation obj) {
         int work_hours = 0, work_days = 0;
         double salary = 0;
-        while (work_hours < MAX_WORK_HOURS && work_days < MAX_WORK_DAYS) {
+        while (work_hours < obj.max_work_hours && work_days < obj.max_work_days) {
             work_days++;
             double empcheck = Math.floor(Math.random() * 10) % 3;
             int check = (int) empcheck;
@@ -26,19 +35,16 @@ public class EmployeeWageComputation {
             }
             System.out.println("Day" + work_days + " Total working hrs " + work_hours);
         }
-        salary = work_hours * PER_HOURS_WAGE;
-        System.out.println(" total working hours " + work_hours + " total working days " + work_days);
+        salary = work_hours * obj.per_hours_wage;
+        System.out.println("company " + obj.company + " total working hours " + work_hours + " total working days " + work_days);
         System.out.println("salary : " + salary);
         return salary;
     }
 
     public static void main(String[] args) {
-        calulateSalary();
+        EmployeeWageComputation dMart = new EmployeeWageComputation("DMart", 20, 25, 100);
+        EmployeeWageComputation tcs = new EmployeeWageComputation("tcs", 25, 25, 100);
+        calulateSalary(dMart);
+        calulateSalary(tcs);
     }
 }
-
-
-
-
-
-
